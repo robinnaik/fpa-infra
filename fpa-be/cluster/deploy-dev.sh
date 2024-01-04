3678dd93739f3a290b0187fb6cc6ce11ca6e1797
Ravin Naik <robin.naik@gmail.com>
2024-01-04 15:37:04 +0530

    Script for terraform deployment

---
+++ fpa-be/cluster/deploy-dev.sh
@@ -0,0 +1,5 @@
+gcloud config set project fpa-be
+terraform init
+terraform validate
+terraform plan
+terraform apply -auto-approve
---
+++ fpa-fe/network/deploy-dev.sh
@@ -0,0 +1,5 @@
+gcloud config set project fpa-fe
+terraform init
+terraform validate
+terraform plan
+terraform apply -auto-approve
