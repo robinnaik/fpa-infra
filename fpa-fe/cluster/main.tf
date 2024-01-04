3c63ef690c934b38c23b4b89748cbce3a68c2be4
Ravin Naik <robin.naik@gmail.com>
2024-01-04 15:39:11 +0530

    Private cluster with external ip for control plane

--- fpa-fe/cluster/main.tf
+++ fpa-fe/cluster/main.tf
@@ -34,6 +34,17 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   deletion_protection=false
   initial_node_count = 1
+
+  private_cluster_config {
+    enable_private_endpoint = false
+    enable_private_nodes   = true 
+    master_ipv4_cidr_block = "10.192.72.16/28"
+  }
+
+  ip_allocation_policy{
+    cluster_secondary_range_name = "${var.project_id}-${var.env}-cluster-ips"
+    services_secondary_range_name = "${var.project_id}-${var.env}-service-ips"
+  }
 }
 
 resource "google_container_node_pool" "primary_cluster_node_pool" {
@@ -43,7 +54,6 @@ resource "google_container_node_pool" "primary_cluster_node_pool" {
     min_node_count = 2
     max_node_count = 10
   }
-
   node_config {
     preemptible  = true
     machine_type = "e2-medium"
@@ -56,7 +66,10 @@ resource "google_container_node_pool" "primary_cluster_node_pool" {
     ]
     labels = {
       app = "fpa"
-      type = "frontend"
+      type = "backend"
+    }
+    metadata = {
+      disable-legacy-endpoints = "true"
     }
   }
 }
