90d5041bb5fd7a648cf39afd944284afcc282854
Ravin Naik <robin.naik@gmail.com>
2024-01-05 18:39:10 +0530

    clean up

--- fpa-be/cluster/variables.tf
+++ fpa-be/cluster/variables.tf
@@ -12,8 +12,3 @@ variable "region" {
   description = "Region for the cluster"
   default = "asia-south1"
 }
-
-variable "gke_service_account" {
-  description = "Service account for cluster"
-  default = "fpa-be-gke-service-account"
-}
