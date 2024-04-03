a94c7d4e04e962a2d406dc786a66a632e9416c53
Ravin Naik <robin.naik@gmail.com>
2024-04-03 19:22:22 +0530

    Removed duplicate env in the name

--- fpa/development/modules/network/network.tf
+++ fpa/development/modules/network/network.tf
@@ -1,10 +1,10 @@
 resource "google_compute_network" "vpc_network" {
-  name                    = "${var.project_id}-${var.env}-network"
+  name                    = "${var.project_id}-network"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "vpc_subnet" {
-  name          = "${var.project_id}-${var.env}-subnet1"
+  name          = "${var.project_id}-subnet1"
   ip_cidr_range = var.primary-subnet-range
   region        = var.region
   network       = google_compute_network.vpc_network.id
